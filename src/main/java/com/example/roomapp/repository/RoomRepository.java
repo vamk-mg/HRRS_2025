5a5cda7314596e4884f281570a1f04af4f1425a1
vamk-mg <goshtaspm@gmail.com>
2025-11-14 00:09:04 +0200

    Delete all files from branch

--- src/main/java/com/example/roomapp/repository/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.roomapp.repository;
-
-import com.example.roomapp.model.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
